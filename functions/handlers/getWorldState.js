0accf352b025d29f5ff8aa84cb47c2b1bb748155
Owlstix <stas.zheleznyak@gmail.com>
2024-08-04 20:25:29 +0200

    Add creation of worldState if record does not exist

--- functions/handlers/getWorldState.js
+++ functions/handlers/getWorldState.js
@@ -3,25 +3,39 @@ const db = admin.firestore();
 
 const getWorldState = async (req, res) => {
   try {
-    const {userId} = req.query;
+    const {userId} = req.body;
 
     if (!userId) {
-      res.status(400).send({error: "User ID is required", query: req.query});
+      res.status(400).send({error: "User ID is required", query: req.body});
       return;
     }
 
     // Find the document ID that corresponds to the user ID
     const userWorldStateQuery = await db.collection("userWorldState").where("userId", "==", userId).get();
 
+    let userWorldState;
+
     if (userWorldStateQuery.empty) {
-      res.status(404).send({error: "User world state not found", world: null});
-      return;
+      // Create a new userWorldState document
+      const newUserWorldState = {
+        userId,
+        food: 20, // Default value
+        supplies: 50, // Default value
+        morale: 100, // Default value
+        passedTutorial: false, // Default value
+      };
+
+      // Add the new document to the collection
+      await db.collection("userWorldState").add(newUserWorldState);
+
+      // Use the newly created state
+      userWorldState = newUserWorldState;
+    } else {
+      // Get the first document from the query result
+      const userWorldStateDoc = userWorldStateQuery.docs[0];
+      userWorldState = userWorldStateDoc.data();
     }
 
-    // Get the first document from the query result
-    const userWorldStateDoc = userWorldStateQuery.docs[0];
-
-    const userWorldState = userWorldStateDoc.data();
     res.status(200).send({
       world: {
         food: userWorldState.food,
